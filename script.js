0b2c6ca7d2141544f7ff7ed460338c84e1d7722e
Peter Gryllis <petergryllis@riacci.com>
2021-12-11 19:19:00 +1100

    generate ASCII character codes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+//Establish the DOM html elements
 var passwordDOM = document.getElementById("password");
 var copybtnDOM = document.getElementById("Copy");
 var lengthDOM = document.getElementById("length");
@@ -5,7 +7,14 @@ var uppercaseDOM = document.getElementById("uppercase");
 var numbersDOM = document.getElementById("numbers");
 var symbolsDOM = document.getElementById("symbols");
 
-
+//generate character does for the Generate Password application using ASCII Character Codes Table
+var UPPERCASE_CODES = arrayFromLowToHigh(65, 90);
+var LOWERCASE_CODES = arrayFromLowToHigh(97, 122);
+var NUMBER_CODES = arrayFromLowToHigh(48, 57);
+var SYMBOL_CODES = arrayFromLowToHigh(33, 47)
+  .concat(arrayFromLowToHigh(58, 64))
+  .concat(arrayFromLowToHigh(91, 96))
+  .concat(arrayFromLowToHigh(123, 126));
 
 
 
